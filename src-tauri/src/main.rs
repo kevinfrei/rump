365f28aa5d194264af8a1f7b21c1cd750e4d7b6a
Kevin Frei <kevinfrei@hotmail.com>
2021-06-12 14:22:37 -0700

    Decent starting skeleton, I guess. Time to learn Rust?

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,83 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Menu, MenuItem};
+
+// Definition in main.rs
+
+struct Database;
+
+#[derive(serde::Serialize)]
+struct CustomResponse {
+  message: String,
+  other_val: usize,
+}
+
+async fn some_other_function() -> Option<String> {
+  Some("response".into())
+}
+
+#[tauri::command]
+async fn my_custom_command(
+  window: tauri::Window,
+  number: usize,
+  _database: tauri::State<'_, Database>,
+) -> Result<CustomResponse, String> {
+  println!("Called from {}", window.label());
+  let result: Option<String> = some_other_function().await;
+  if let Some(message) = result {
+    Ok(CustomResponse {
+      message,
+      other_val: 42 + number,
+    })
+  } else {
+    Err("No result".into())
+  }
+}
+
 fn main() {
+  let menu = vec![
+    // on macOS first menu is always app name
+    Menu::new(
+      "RuMP",
+      vec![
+        MenuItem::Services,
+        MenuItem::Separator,
+        MenuItem::Hide,
+        MenuItem::HideOthers,
+        MenuItem::ShowAll,
+        MenuItem::Separator,
+        MenuItem::Quit,
+        MenuItem::CloseWindow,
+      ],
+    ),
+    Menu::new(
+      "Edit",
+      vec![
+        MenuItem::Undo,
+        MenuItem::Redo,
+        MenuItem::Separator,
+        MenuItem::Cut,
+        MenuItem::Copy,
+        MenuItem::Paste,
+        MenuItem::Separator,
+        MenuItem::SelectAll,
+      ],
+    ),
+  ];
   tauri::Builder::default()
+    .manage(Database {})
+    .menu(menu)
+/*    .on_menu_event(|event| match event.menu_item_id().as_str() {
+      "quit" => {
+        std::process::exit(0);
+      }
+      "close" => {
+        event.window().close().unwrap();
+      }
+      _ => {}
+    })*/
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api/tauri';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to magically reload.
         </p>
         <a
           className="App-link"
@@ -16,9 +16,33 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          {/* <audio src="Razor.flac" autoPlay={true}/> */ }
           Learn React
         </a>
+        <div id="res"/>
+        <div id="err"/>
       </header>
+      <button onClick={() =>
+        invoke('my_custom_command', {number: 42})
+          .then((res:any) => {
+            const r = document.getElementById('res');
+            if (!r) return;
+            let str = "Found Res. ";
+            if (res.message) {
+              str += res.message;
+            }
+            if (res.other_val) {
+              str += res.other_val;
+            }
+            r.innerText += "\n" + str;
+          })
+          .catch((e) => {
+            const el = document.getElementById('err');
+            if (el) {
+              el.innerText = e.toString();
+            }
+          })
+      }>Click Me</button>
     </div>
   );
 }
